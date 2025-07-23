52cc675c2c32bf2e68a3ccd5e3d7887c91085dc0
Diksha1312 <gdiksha1312@gmail.com>
2025-07-24 00:23:37 +0200

    update block component

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying" >Start Game</button>
+    <BlockComp v-if="isPlaying" :delay="delay" />
+  </div>
+</template>
+<script>
+
+import BlockComp from './components/BlockComp.vue';
+export default {
+  name: 'App',
+  components: { BlockComp },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+        this.delay = 2000 + Math.random() * 5000; // Random delay between 2 and 7 seconds
+        this.isPlaying = true;
+        console.log("this.delay", this.delay);
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/BlockComp.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        Click Me!
+    </div>
+</template>
+
+<script lang="js">
+export default {
+    props: ['delay'], // Delay in milliseconds before the block appears
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay);
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(`Reaction Time: ${this.reactionTime} ms`)
+        }
+    }
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    background: #42b983;
+    text-align: center;
+    color: white;
+    padding: 100px 0;
+    margin: 40px auto;
+    border-radius: 20px;
+}
+</style>
